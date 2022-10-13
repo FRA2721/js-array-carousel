6fb5e78c72cfcda7da9d853eb7b2f0d627a1ac0e
FRA2721 <cimino.francesco36@gmail.com>
2022-10-13 21:46:58 +0200

    carousel completed (remains some css formatting actions)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,95 @@
 // programmer: Francesco Cimino;
-// project: Carousel Mono Array;
+// project: Carousel Mono Arra;
 // language: javascript;
+
+// imgs-list
+const imgsList = [
+    "img/01.jpg",
+    "img/02.jpg",
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg",
+];
+
+const card = document.querySelector(".left-section");
+const cardRight = document.querySelector(".right-section");
+
+// items
+for (let i = 0; i < imgsList.length; i++) {
+    let picPosition = imgsList[i];
+    const items =
+            `<div class="card-img-1">
+                <img src="${imgsList[i]}" alt="environment images" >
+            </div>`;
+
+    const itemsRight =
+            `<div class="small-img">
+                <img src="${imgsList[i]}" alt="small environment images">
+            </div>`;
+
+    card.innerHTML += items;
+    cardRight.innerHTML += itemsRight;
+}
+
+// active-class-section
+const cardImg = document.getElementsByClassName("card-img-1");
+const cardSmallImg = document.getElementsByClassName("small-img");
+
+let arrowPointer = 0;
+
+cardImg[arrowPointer].classList.add("active");
+cardSmallImg[arrowPointer].classList.add("active");
+
+//onclick-section
+const downArrowBtn = document.querySelector(".down-arrow");
+const upArrowBtn = document.querySelector(".up-arrow");
+
+// onclick-down-arrow-btn
+downArrowBtn.addEventListener("click", function() {
+    
+    cardImg[arrowPointer].classList.remove("active");
+    cardSmallImg[arrowPointer].classList.remove("active");
+
+    if (arrowPointer < 4){
+        arrowPointer++;
+
+    } else {
+        arrowPointer = 0;
+    }
+
+    cardImg[arrowPointer].classList.add("active");
+    cardSmallImg[arrowPointer].classList.add("active");
+});
+
+// onclick-up-arrow-btn
+ upArrowBtn.addEventListener("click", function() {
+
+    cardImg[arrowPointer].classList.remove("active");
+    cardSmallImg[arrowPointer].classList.remove("active");
+
+    if (arrowPointer > 0){
+        arrowPointer--;
+    } else {
+
+        arrowPointer = 4;
+    }
+
+    cardImg[arrowPointer].classList.add("active");
+    cardSmallImg[arrowPointer].classList.add("active");
+});
+
+// loop
+for (let i = 0; i < cardSmallImg.length; i++) {
+    const items = cardSmallImg[i];
+    items.addEventListener("click", function() {
+
+        cardImg[arrowPointer].classList.remove("active");
+        cardSmallImg[arrowPointer].classList.remove("active");
+        
+        arrowPointer = i;
+
+        cardImg[arrowPointer].classList.add("active");
+        cardSmallImg[arrowPointer].classList.add("active")
+        
+    });  
+}
